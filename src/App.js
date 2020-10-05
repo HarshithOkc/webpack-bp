bb10a036c93bf8af4403bc8c85bfea0618cd2ef7
HarshithOkc <harshith@okcredit.in>
2020-10-05 10:08:44 +0530

    workbox init

--- src/App.js
+++ src/App.js
@@ -4,20 +4,6 @@ import AppRouter from './utils/Routes/Public';
 const path = require('path');
 
 const App = () => {
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function() {
-          navigator.serviceWorker.register('./sw.js').then(function(registration) {
-            
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-          }, function(err) {
-            
-            console.log('ServiceWorker registration failed: ', err);
-          });
-        });
-    }
-    
-    console.log(ENV);
-
     return(
         <AppRouter />
     )
